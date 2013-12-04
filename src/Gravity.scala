60bda59dfed03bd1d08fd0c0186d93b20f320b75
Xiaohan Zhang <hellocharlien@hotmail.com>
2013-12-04 05:02:08 -0800

    Multithreading with par collections; much faster

--- src/Gravity.scala
+++ src/Gravity.scala
@@ -3,54 +3,52 @@ import org.zhang.lib._
 
 class Gravity extends MyPApplet {
 
-  import PApplet._;
-  import PConstants._;
+  import PApplet._
+  import PConstants._
 
   val mouse = new PVector()
   var pow = 1
   class Dot(val pos: PVector, val vel: PVector = new PVector()) {
     def run() {
+      if(mousePressed) {
+        val dx = pos.x - mouse.x
+        val dy = pos.y - mouse.y
+        val mag = sqrt(dx*dx + dy*dy)
+        vel.add(pow * dx / mag, pow * dy / mag, 0)
+      }
+
       vel.mult(.98f)
       pos.add(vel)
+      draw()
     }
 
-    def push() {
-      val dx = pos.x - mouse.x
-      val dy = pos.y - mouse.y
-      val mag = sqrt(dx*dx + dy*dy)
-      vel.add(pow * dx / mag, pow * dy / mag, 0)
+    def draw() {
+      if(!(pos.x < 0 || pos.y < 0 || pos.x >= width || pos.y >= height)) {
+        val idx = pos.y.toInt * width + pos.x.toInt
+        //this line has a non-deterministic condition in it but it doesn't make a difference visually
+        pixels(idx) = colorCache(pixels(idx) & 0xFF) // get the blue component (which is the same as red, and green, and the brightness)
+      }
     }
   }
 
-  lazy val dots = Array.tabulate(500000)(i => new Dot(new PVector(i * width / 500000f, height/2)))
+  lazy val dots = Array.tabulate(1000000)(i => new Dot(new PVector(i * width / 1000000f, height/2)))
+  lazy val parDots = dots.par
+
+  val cellAlpha = 14
+  lazy val colorCache = Array.tabulate(255)(b => color((b + (255 - b) * (cellAlpha / 255f)).toInt))
 
   override def setup() {
-    size(1000, 600)
+    size(displayWidth, displayHeight)
   }
 
   override def draw() {
+    background(0)
+    loadPixels()
     if(mousePressed) {
       mouse.set(mouseX, mouseY)
       pow = if(mouseButton == LEFT) -1 else 1
-      for(d <- dots) d.push()
     }
-    background(0)
-    loadPixels()
-    val cellAlpha = 14
-
-//    println(dots.getClass)
-    var i = 0
-    while(i < dots.length) {
-      val d = dots(i)
-      d.run()
-      if(!(d.pos.x < 0 || d.pos.y < 0 || d.pos.x >= width || d.pos.y >= height)) {
-        val idx = d.pos.y.toInt * width + d.pos.x.toInt
-        val b = pixels(idx) & 0xFF
-        val nextB = (b + (255 - b) * (cellAlpha / 255f)).toInt
-        pixels(idx) = color(nextB)
-      }
-      i += 1
-    }
+    parDots.foreach(_.run())
     updatePixels()
     println(frameRate)
   }
